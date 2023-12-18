9e973bd2bf5a35eb351ab5ee912d60b0fad492ab
ZerProg <154282810+ZerProg-Studio@users.noreply.github.com>
2023-12-19 02:47:00 +0300

    Update prot.js

--- index/Folder/Boot/prot.js
+++ index/Folder/Boot/prot.js
@@ -1,3 +1,3 @@
 window.addEventListener("mousedown", (e)=>{if(e.button==2){
-    // alert('Copyright Zer Prog studio. All rights reserved.')
+     alert('Copyright Zer Prog studio. All rights reserved.')
 }});
